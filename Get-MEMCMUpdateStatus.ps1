b558370a2f40194edf3fe3b09e031c2d9d90bf8c
chida <christian@damberg.org>
2023-09-26 16:13:34 +0200

    Update Get-MEMCMUpdateStatus.ps1

--- Get-MEMCMUpdateStatus.ps1
+++ Get-MEMCMUpdateStatus.ps1
@@ -7,8 +7,17 @@
     write-host -ForegroundColor Green 'PSWriteHtml imported'
 
 $siteserver = 'vntsql0299'
+$filedate = get-date -Format yyyMMdd
+$HTMLFileSavePath = "c:\temp\KVV_UpdateStatus_$filedate.HTML"
+$CSVFileSavePath = "c:\temp\KVV_UpdateStatus_$filedate.csv"
 
+$SMTP = 'smtp.kvv.se'
+$MailFrom = 'no-reply@kvv.se'
+$MailTo = 'christian.damberg@kriminalvarden.se'
+$MailPortnumber = '25'
+$MailCustomer = 'Kriminalvården - IT'
 
+$ResultColl = @()
 
 #region Functions needed in script
 
@@ -234,7 +243,7 @@ if ($todayCompare -eq $ReportdayCompare)
 
 {
 
-    Get-SCCMSoftwareUpdateStatus -DeploymentID 16777364
+    $UpdateStatus = Get-SCCMSoftwareUpdateStatus -DeploymentID 16777364
 
 }
 
@@ -245,3 +254,211 @@ else
     write-host "date not equal"
 
 }
+
+foreach ($US in $UpdateStatus)
+{
+
+                                $object = New-Object -TypeName PSObject
+                                $object | Add-Member -MemberType NoteProperty -Name 'Server' -Value $us.DeviceName
+                                $object | Add-Member -MemberType NoteProperty -Name 'Collection' -Value $us.CollectionName
+                                $object | Add-Member -MemberType NoteProperty -Name 'Status' -Value $us.Status
+                                $object | Add-Member -MemberType NoteProperty -Name 'StatusTid' -Value $us.StatusTime
+
+                                $resultColl += $object
+
+
+
+}
+
+
+
+New-HTML -TitleText "Uppdatering Status - Kriminalvården" -FilePath $HTMLFileSavePath -ShowHTML -Online {
+
+    New-HTMLHeader {
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLText -Text "Kriminalvården - UpdateStatus" -FontSize 35 -Color Darkblue -FontFamily Arial -Alignment center
+                New-HTMLHorizontalLine
+            }
+        }
+    }
+
+    New-HTMLSection -Invisible -Title "UpdateStatus $filedate"{
+
+        New-HTMLTable -DataTable $resultColl -PagingLength 25 -Style compact
+        
+    }
+
+    New-HTMLFooter {
+    
+        New-HTMLSection -Invisible {
+
+                    New-HTMLPanel -Invisible {
+                New-HTMLHorizontalLine
+                New-HTMLText -Text "Denna lista skapades $today" -FontSize 20 -Color Darkblue -FontFamily Arial -Alignment center -FontStyle italic
+            }
+            
+        }
+    }
+}
+
+
+$Body = @"
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Uppdatering Status - Kriminalvården</title>
+<style>
+
+    th {
+
+        font-family: Arial, Helvetica, sans-serif;
+        color: White;
+        font-size: 12px;
+        border: 1px solid black;
+        padding: 3px;
+        background-color: Black;
+
+    } 
+    p {
+
+        font-family: Arial, Helvetica, sans-serif;
+        color: black;
+        font-size: 12px;
+
+    } 
+    ol {
+
+        font-family: Arial, Helvetica, sans-serif;
+        list-style-type: square;
+        color: black;
+        font-size: 12px;
+
+    }
+	    H1 {
+
+        font-family: Arial, Helvetica, sans-serif;
+        color: black;
+        font-size: 18px;
+
+    }
+    tr {
+
+        font-family: Arial, Helvetica, sans-serif;
+        color: black;
+        font-size: 11px;
+        vertical-align: text-top;
+
+    } 
+
+    body {
+        background-color: lightgray;
+      }
+      table {
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+
+      td {
+        border: 1px solid black;
+        padding: 5px;
+        background-color: #E0F3F7;
+      }
+
+</style>
+</head>
+
+<body>
+	<p><h1>Uppdatering Status</h1></p> 
+	<p>Bifogade filer innehåller Status för aktuell update Grupp.<br><br>
+<hr>
+</p> 
+	<p>Report created $((Get-Date).ToString()) from <b><i>$($Env:Computername)</i></b></p>
+
+	
+	
+	
+</body>
+</html>
+ 
+
+"@
+
+
+
+
+#endregion
+
+#Region Mailsettings
+
+
+#use secure connection if available ([bool], optional)
+$UseSecureConnectionIfAvailable=$false
+
+#authentication ([System.Management.Automation.PSCredential], optional)
+$Credential=[System.Management.Automation.PSCredential]::new("Username", (ConvertTo-SecureString -String "Password" -AsPlainText -Force))
+
+#SMTP server ([string], required)
+$SMTPServer=$SMTP
+
+#port ([int], required)
+$Port=$MailPortnumber
+
+#sender ([MimeKit.MailboxAddress] http://www.mimekit.net/docs/html/T_MimeKit_MailboxAddress.htm, required)
+$From=[MimeKit.MailboxAddress]$MailFrom
+
+#recipient list ([MimeKit.InternetAddressList] http://www.mimekit.net/docs/html/T_MimeKit_InternetAddressList.htm, required)
+$RecipientList=[MimeKit.InternetAddressList]::new()
+$RecipientList.Add([MimeKit.InternetAddress]$MailTo)
+
+
+#cc list ([MimeKit.InternetAddressList] http://www.mimekit.net/docs/html/T_MimeKit_InternetAddressList.htm, optional)
+#$CCList=[MimeKit.InternetAddressList]::new()
+#$CCList.Add([MimeKit.InternetAddress]$EmailToCC)
+
+
+
+#bcc list ([MimeKit.InternetAddressList] http://www.mimekit.net/docs/html/T_MimeKit_InternetAddressList.htm, optional)
+$BCCList=[MimeKit.InternetAddressList]::new()
+$BCCList.Add([MimeKit.InternetAddress]"BCCRecipient1EmailAddress")
+
+
+#subject ([string], required)
+$Subject=[string]"Serverpatchning $MailCustomer $monthname $year"
+
+#text body ([string], optional)
+#$TextBody=[string]"TextBody"
+
+#HTML body ([string], optional)
+$HTMLBody=[string]$Body
+
+#attachment list ([System.Collections.Generic.List[string]], optional)
+$AttachmentList=[System.Collections.Generic.List[string]]::new()
+$AttachmentList.Add("$HTMLFileSavePath")
+$AttachmentList.Add("$CSVFileSavePath")
+
+# Mailparameters
+$Parameters=@{
+    "UseSecureConnectionIfAvailable"=$UseSecureConnectionIfAvailable    
+    #"Credential"=$Credential
+    "SMTPServer"=$SMTPServer
+    "Port"=$Port
+    "From"=$From
+    "RecipientList"=$RecipientList
+    #"CCList"=$CCList
+    #"BCCList"=$BCCList
+    "Subject"=$Subject
+    #"TextBody"=$TextBody
+    "HTMLBody"=$HTMLBody
+    "AttachmentList"=$AttachmentList
+}
+
+#endregion
+
+#Region Send Mail
+
+Send-MailKitMessage @Parameters
+
+set-location $PSScriptRoot
